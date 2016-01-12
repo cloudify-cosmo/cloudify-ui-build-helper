02257b089f1f325244b5aeca7f877af2809772cf
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2016-01-12 14:26:58 +0200

    change script name reference build_uid instead of build_id

--- get_stable_s3_folder.sh
+++ get_stable_s3_folder.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 
-if [ "${VERSION}" == "" ];then
+if [ "${CFY_VERSION}" == "" ];then
     echoerr "get_unstable_s3_dist :: VERSION expected but missing"
     exit 1
 fi
 
-if [ "${PRERELEASE}" == "" ];then
+if [ "${CFY_PRERELEASE}" == "" ];then
     echoerr "get_unstable_s3_dist :: PRERELEASE expected but missing"
     exit 1
 fi
 
-if [ "${BUILD_NUMBER}" == "" ];then
+if [ "${CFY_BUILD_NUMBER}" == "" ];then
     echoerr "get_unstable_s3_dist :: BUILD_NUMBER expected but missing"
     exit 1
 fi
 
-if [ "${BUILD_ID}" == "" ];then
+if [ "${BUILD_UID}" == "" ];then
     echoerr "get_unstable_s3_dist :: BUILD_ID expected but missing"
     exit 1
 fi
@@ -26,4 +26,4 @@ if [ "${PROJECT_NAME}" == "" ];then
     exit 1
 fi
 
-echo "continuous-build/stable/${PROJECT_NAME}/${VERSION}-${PRERELEASE}-${BUILD_ID}"
+echo "continuous-build/stable/${PROJECT_NAME}/${CFY_VERSION}-${CFY_PRERELEASE}-${CFY_BUILD_UID}"
