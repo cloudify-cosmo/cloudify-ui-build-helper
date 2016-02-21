48dacd5a4709bf4b1fdf972e575330547ddd036d
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2016-02-21 17:26:10 +0200

    add url to unstable composer

--- get_artifacts_files.sh
+++ get_artifacts_files.sh
@@ -3,7 +3,7 @@
 set -e
 
 export CLOUDIFY_UI_TAR_GZ="cloudify-ui-${CFY_VERSION}-${CFY_PRERELEASE}-b${CFY_BUILD_NUMBER}.tgz"
-export CLOUDIFY_COMPOSR_TAR_GZ="blueprintcomposer-${CFY_VERSION}.tgz"
+export CLOUDIFY_COMPOSER_TAR_GZ="blueprintcomposer-${CFY_VERSION}.tgz"
 
 
-export CLOUDIFY_COMPOSER_UNSTABLE_URL="https://s3.amazonaws.com/cloudify-ui/`PROJECT_NAME=cloudify-blueprint-composer get-unstable-s3-folder`/${CLOUDIFY_COMPOSR_TAR_GZ}"
+export CLOUDIFY_COMPOSER_UNSTABLE_URL="https://s3.amazonaws.com/cloudify-ui/`PROJECT_NAME=cloudify-blueprint-composer get-unstable-s3-folder`/${CLOUDIFY_COMPOSER_TAR_GZ}"
